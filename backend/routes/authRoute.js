1a072999fef4bcb663de232a27ce98f31bfb058b
Aisha-Ahmad-025 <aishaahmad0265@gmail.com>
2026-08-26 21:27:41 +0500

    created another middleware for admin role

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import { registerUser, loginUser, getUsers } from '../controller/authController.js'
 import protect from '../middleware/authMiddleware.js'
+import admin from '../middleware/adminMiddleware.js'
 const router = express.Router()
 
+
+// into the models we have a generate token function and here we are checking it via protect middleware
+//also thw admin can see all the user by using the admin middleware
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.get('/users', protect, admin, getUsers)
